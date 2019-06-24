1372048f2961ab03bed930d5f8574a3cdcbce4d0
Kévin SCHNEKENBURGER <kschnekenburger@comparadise.com>
2019-06-24 22:17:13 +0200

    Implement IDE Stubs 4.x & delete old resources

--- src/Phalcon/acl/Adapter.php
+++ src/Phalcon/acl/Adapter.php
@@ -2,54 +2,57 @@
 
 namespace Phalcon\Acl;
 
+use Phalcon\Events\EventsAwareInterface;
+use Phalcon\Events\ManagerInterface;
+
 /**
  * Phalcon\Acl\Adapter
  *
  * Adapter for Phalcon\Acl adapters
  */
-abstract class Adapter implements \Phalcon\Acl\AdapterInterface, \Phalcon\Events\EventsAwareInterface
+abstract class Adapter implements AdapterInterface, EventsAwareInterface
 {
     /**
      * Events manager
      *
      * @var mixed
      */
-    protected $_eventsManager;
+    protected $eventsManager;
 
     /**
      * Default access
      *
      * @var bool
      */
-    protected $_defaultAccess = true;
+    protected $defaultAccess = true;
 
     /**
      * Access Granted
      *
      * @var bool
      */
-    protected $_accessGranted = false;
+    protected $accessGranted = false;
 
     /**
      * Role which the list is checking if it's allowed to certain resource/access
      *
      * @var string
      */
-    protected $_activeRole;
+    protected $activeRole;
 
     /**
      * Resource which the list is checking if some role can access it
      *
      * @var string
      */
-    protected $_activeResource;
+    protected $activeComponent;
 
     /**
      * Active access which the list is checking if some role can access it
      *
      * @var string
      */
-    protected $_activeAccess;
+    protected $activeAccess;
 
 
     /**
@@ -57,48 +60,48 @@ abstract class Adapter implements \Phalcon\Acl\AdapterInterface, \Phalcon\Events
      *
      * @return string
      */
-    public function getActiveRole() {}
+    public function getActiveRole() : string {}
 
     /**
      * Resource which the list is checking if some role can access it
      *
      * @return string
      */
-    public function getActiveResource() {}
+    public function getActiveComponents() {}
 
     /**
      * Active access which the list is checking if some role can access it
      *
      * @return string
      */
-    public function getActiveAccess() {}
+    public function getActiveAccess() : string {}
 
     /**
      * Sets the events manager
      *
-     * @param \Phalcon\Events\ManagerInterface $eventsManager
+     * @param ManagerInterface $eventsManager
      */
-    public function setEventsManager(\Phalcon\Events\ManagerInterface $eventsManager) {}
+    public function setEventsManager(ManagerInterface $eventsManager) {}
 
     /**
      * Returns the internal event manager
      *
-     * @return \Phalcon\Events\ManagerInterface
+     * @return ManagerInterface
      */
-    public function getEventsManager() {}
+    public function getEventsManager() : ManagerInterface {}
 
     /**
      * Sets the default access level (Phalcon\Acl::ALLOW or Phalcon\Acl::DENY)
      *
      * @param int $defaultAccess
      */
-    public function setDefaultAction($defaultAccess) {}
+    public function setDefaultAction(int $defaultAccess) : void {}
 
     /**
      * Returns the default ACL access level
      *
      * @return int
      */
-    public function getDefaultAction() {}
+    public function getDefaultAction() : int {}
 
 }
--- src/Phalcon/acl/AdapterInterface.php
+++ src/Phalcon/acl/AdapterInterface.php
@@ -11,161 +11,142 @@ interface AdapterInterface
 {
 
     /**
-     * Sets the default access level (Phalcon\Acl::ALLOW or Phalcon\Acl::DENY)
-     *
-     * @param int $defaultAccess
+     * Do a role inherit from another existing role
+     * @param string $roleName
+     * @param $roleToInherit
+     * @return bool
      */
-    public function setDefaultAction($defaultAccess);
+    public function addInherit(string $roleName, $roleToInherit) : bool;
+
+    /**
+     * Adds a role to the ACL list. Second parameter lets to inherit access data
+     * from other existing role
+     * @param $role
+     * @param null $accessInherits
+     * @return bool
+     */
+    public function addRole($role, $accessInherits = null) : bool;
+
+    /**
+     * Adds a component to the ACL list
+     *
+     * Access names can be a particular action, by example
+     * search, update, delete, etc or a list of them
+     * @param $componentObject
+     * @param $accessList
+     * @return bool
+     */
+    public function addComponent($componentObject, $accessList) : bool;
+
+    /**
+     * Adds access to components
+     * @param $componentName
+     * @param $accessList
+     * @return bool
+     */
+    public function addComponentAccess(string $componentName, $accessList) : bool;
+
+    /**
+     * Allow access to a role on a component
+     * @param $roleName
+     * @param $componentName
+     * @param $access
+     * @param null $func
+     */
+    public function allow(string $roleName, string $componentName, $access, $func = null) : void;
+
+    /**
+     * Deny access to a role on a component
+     * @param $roleName
+     * @param $componentName
+     * @param $access
+     * @param null $func
+     */
+    public function deny(string $roleName, string $componentName, $access, $func = null) : void;
+
+    /**
+     * Removes an access from a component
+     * @param string $componentName
+     * @param $accessList
+     */
+    public function dropComponentAccess(string $componentName, $accessList) : void;
+
+    /**
+     * Returns the access which the list is checking if some role can access it
+     */
+    public function getActiveAccess() : string;
+
+    /**
+     * Returns the role which the list is checking if it's allowed to certain
+     * component/access
+     */
+    public function getActiveRole() : string;
+
+    /**
+     * Returns the component which the list is checking if some role can access
+     * it
+     */
+    public function getActiveComponent() : string;
 
     /**
      * Returns the default ACL access level
-     *
-     * @return int
      */
-    public function getDefaultAction();
-
-    /**
-     * Sets the default access level (Phalcon\Acl::ALLOW or Phalcon\Acl::DENY)
-     * for no arguments provided in isAllowed action if there exists func for accessKey
-     *
-     * @param int $defaultAccess
-     */
-    public function setNoArgumentsDefaultAction($defaultAccess);
+    public function getDefaultAction() : int;
 
     /**
      * Returns the default ACL access level for no arguments provided in
      * isAllowed action if there exists func for accessKey
-     *
-     * @return int
      */
-    public function getNoArgumentsDefaultAction();
-
-    /**
-     * Adds a role to the ACL list. Second parameter lets to inherit access data from other existing role
-     *
-     * @param mixed $role
-     * @param mixed $accessInherits
-     * @return bool
-     */
-    public function addRole($role, $accessInherits = null);
-
-    /**
-     * Do a role inherit from another existing role
-     *
-     * @param string $roleName
-     * @param mixed $roleToInherit
-     * @return bool
-     */
-    public function addInherit($roleName, $roleToInherit);
-
-    /**
-     * Check whether role exist in the roles list
-     *
-     * @param string $roleName
-     * @return bool
-     */
-    public function isRole($roleName);
-
-    /**
-     * Check whether resource exist in the resources list
-     *
-     * @param string $resourceName
-     * @return bool
-     */
-    public function isResource($resourceName);
-
-    /**
-     * Adds a resource to the ACL list
-     *
-     * Access names can be a particular action, by example
-     * search, update, delete, etc or a list of them
-     *
-     * @param mixed $resourceObject
-     * @param mixed $accessList
-     * @return bool
-     */
-    public function addResource($resourceObject, $accessList);
-
-    /**
-     * Adds access to resources
-     *
-     * @param string $resourceName
-     * @param mixed $accessList
-     */
-    public function addResourceAccess($resourceName, $accessList);
-
-    /**
-     * Removes an access from a resource
-     *
-     * @param string $resourceName
-     * @param mixed $accessList
-     */
-    public function dropResourceAccess($resourceName, $accessList);
-
-    /**
-     * Allow access to a role on a resource
-     *
-     * @param string $roleName
-     * @param string $resourceName
-     * @param mixed $access
-     * @param mixed $func
-     */
-    public function allow($roleName, $resourceName, $access, $func = null);
-
-    /**
-     * Deny access to a role on a resource
-     *
-     * @param string $roleName
-     * @param string $resourceName
-     * @param mixed $access
-     * @param mixed $func
-     */
-    public function deny($roleName, $resourceName, $access, $func = null);
-
-    /**
-     * Check whether a role is allowed to access an action from a resource
-     *
-     * @param mixed $roleName
-     * @param mixed $resourceName
-     * @param string $access
-     * @param array $parameters
-     * @return bool
-     */
-    public function isAllowed($roleName, $resourceName, $access, array $parameters = null);
-
-    /**
-     * Returns the role which the list is checking if it's allowed to certain resource/access
-     *
-     * @return string
-     */
-    public function getActiveRole();
-
-    /**
-     * Returns the resource which the list is checking if some role can access it
-     *
-     * @return string
-     */
-    public function getActiveResource();
-
-    /**
-     * Returns the access which the list is checking if some role can access it
-     *
-     * @return string
-     */
-    public function getActiveAccess();
+    public function getNoArgumentsDefaultAction() : int;
 
     /**
      * Return an array with every role registered in the list
-     *
      * @return RoleInterface[]
      */
-    public function getRoles();
+    public function getRoles() : array;
 
     /**
-     * Return an array with every resource registered in the list
-     *
-     * @return ResourceInterface[]
+     * Return an array with every component registered in the list
+     * return ComponentInterface[]
      */
-    public function getResources();
+    public function getComponents() : array;
+
+    /**
+     * Check whether a role is allowed to access an action from a component
+     * @param $roleName
+     * @param $componentName
+     * @param string $access
+     * @param array|null $parameters
+     * @return bool
+     */
+    public function isAllowed($roleName, $componentName, string $access, ?array $parameters = null) : bool;
+
+    /**
+     * Check whether component exist in the components list
+     * @param string $componentName
+     * @return bool
+     */
+    public function isComponent(string $componentName) : bool;
+
+    /**
+     * Check whether role exist in the roles list
+     * @param string $roleName
+     * @return bool
+     */
+    public function isRole(string $roleName) : bool;
+
+    /**
+     * Sets the default access level (Phalcon\Acl::ALLOW or Phalcon\Acl::DENY)
+     * @param int $defaultAccess
+     */
+    public function setDefaultAction(int $defaultAccess) : void;
+
+    /**
+     * Sets the default access level (Phalcon\Acl::ALLOW or Phalcon\Acl::DENY)
+     * for no arguments provided in isAllowed action if there exists func for
+     * accessKey
+     * @param int $defaultAccess
+     */
+    public function setNoArgumentsDefaultAction(int $defaultAccess) : void;
 
 }
