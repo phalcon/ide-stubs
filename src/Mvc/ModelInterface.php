25ff67b573d579877b446d588b8ca28ec9ac72e3
Anton Vasiliev <aionbase@gmail.com>
2021-09-11 10:15:58 +0100

    Fix return type in ModelInterface

--- src/Mvc/ModelInterface.php
+++ src/Mvc/ModelInterface.php
@@ -282,7 +282,7 @@ interface ModelInterface
      * constants
      *
      * @param int $dirtyState
-     * @return 0|ModelInterface
+     * @return bool|ModelInterface
      */
     public function setDirtyState(int $dirtyState);
 
